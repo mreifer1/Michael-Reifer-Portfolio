db684a7d8d2500e89f842636a3da39f4ccbf9806
Michael Reifer <Mjreifer1@gmail.com>
2025-05-26 10:23:59 -0400

    new changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,17 @@
+import { ThemeToggle } from "../components/ThemeToggle"
+
+
 export const Home = () => {
-    return <div> Home Page </div>
+    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+        {/* Handle our theme*/}
+        <ThemeToggle />
+        {/* Lets put background here*/}
+
+        {/* Navbar*/}
+
+        {/* Main content*/}
+
+
+        {/* Footer */}
+    </div>
 }
