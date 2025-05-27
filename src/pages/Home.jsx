ba881f95b6af55f0035c9d40fa66b2c819890622
Michael Reifer <Mjreifer1@gmail.com>
2025-05-27 08:01:09 -0400

    big changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { ThemeToggle } from "../components/ThemeToggle"
 import { StarBackground } from "../components/StarBackground"
-
+import { Navbar } from "../components/Navbar"
+import { HeroSection } from "../components/HeroSection"
+import { AboutSection } from "../components/AboutSection"
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -8,10 +10,15 @@ export const Home = () => {
         <ThemeToggle />
         {/* Background effects here*/}
         <StarBackground />
+
+
         {/* Navbar*/}
-
+        <Navbar />
         {/* Main content*/}
-
+        <main>
+            <HeroSection />
+            <AboutSection />
+        </main>
 
         {/* Footer */}
     </div>
